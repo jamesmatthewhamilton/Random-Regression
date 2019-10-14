e5f742e5803f6963e96fd1a393f646342b383e53
James Matthew Hamilton <j.hamilton@gatech.edu>
2019-10-14 00:20:44 -0400

    Improved cutoff test which now alters cutoffs at a linear rate.

--- Tests/Accuracy_Tests/CutOff.R
+++ Tests/Accuracy_Tests/CutOff.R
@@ -3,30 +3,30 @@ if("RandomLasso" %in% (.packages())){detach("package:RandomLasso", unload = TRUE
 install.packages("../../RandomLasso/", repos = NULL, type = "source")
 library(RandomLasso)
 
-ITERATIONS = 5
-TESTS = 100
-CORES = 16
-COLNAMES = rep(1, 100)
+ITERATIONS = 10
+TESTS = 101
+CORES = TRUE
+COLNAMES = rep(1, 101)
 
 source("../func/SimulateTestData.R")
-s <- SimulateTestData("../res/sim4_sig3_our.RData", TESTS, ITERATIONS)
+s <- SimulateTestData("../res/sim3_sig3_our.RData", TESTS, ITERATIONS)
 pb <- txtProgressBar(min = 0, max = (TESTS * ITERATIONS), style = 3)
 
 for (ii in 1:ITERATIONS) {
-    for (jj in 1:TESTS) {
-        s$coef[[jj]][,ii] <- HiLassoC(s$x[[ii]], s$y[[ii]],
-                                    alpha = c(0.5, 1),
-                                    cutoff = c(1 / jj^2, 0, 0),
-                                    cores = CORES,
-                                    verbose = FALSE)
+    for (jj in 0:TESTS) {
+        s$coef[[jj + 1]][,ii] <- HiLassoC(s$x[[ii]], s$y[[ii]], alpha = c(0.5, 1),
+                                      cutoff = ifelse(jj != TESTS,
+				      c(1-(jj*0.0095), 0, 0),
+				      c(0, 0, 0)),
+                                      cores = CORES, verbose = FALSE)
         setTxtProgressBar(pb, ((ii - 1) * TESTS + jj))
-        if(ii == 1) {COLNAMES[jj] = COLNAMES[jj] / jj^2}
+        if(ii == 1) {COLNAMES[jj + 1] = 1-(jj*0.0095)}
     }
 }
-
+COLNAMES[TESTS] = 0
 
 source("../func/RunAccuracyTests.R")
-r <- RunAccuracyTest(s, "../log", "Cutoff", TESTS, ITERATIONS, COLNAMES)
+r <- RunAccuracyTest(s, "../log", "Cutoff[1-0.05]_at_Part1", TESTS, ITERATIONS, COLNAMES)
 
 source("../func/VisualizeResults.R")
-VisualizeResults(r, s, "Cutoff at Part I", angle = 45)
+VisualizeResults(r, s, "Cutoff[1-0.05] at Part I", angle = 45)
