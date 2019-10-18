d5369b5f14d835f63a8f3de6c26ca8e6573e566a
James Matthew Hamilton <j.hamilton@gatech.edu>
2019-10-18 00:00:00 -0400

    Most up-to-date Box Ratio test.

--- Tests/Accuracy_Tests/BoxRatio.R
+++ Tests/Accuracy_Tests/BoxRatio.R
@@ -3,23 +3,22 @@ if("RandomLasso" %in% (.packages())){detach("package:RandomLasso", unload = TRUE
 install.packages("../../RandomLasso/", repos = NULL, type = "source")
 library(RandomLasso)
 
-ITERATIONS = 5
-TESTS = 47
+ITERATIONS = 10
+TESTS = 90
 CORES = 16
-COLNAMES = seq(245, 15, -5) / 200
+COLNAMES = seq(550, 100, -5) 500
 
 source("../func/SimulateTestData.R")
-s <- SimulateTestData("../res/sim3_sig3_our.RData", TESTS, ITERATIONS)
+s <- SimulateTestData("../res/sim4_sig3_our.RData", TESTS, ITERATIONS)
 pb <- txtProgressBar(min = 0, max = (TESTS * ITERATIONS), style = 3)
 
 for (ii in 1:ITERATIONS) {
     for (jj in 1:TESTS) {
         s$coef[[jj]][,ii] <- HiLasso(s$x[[ii]], s$y[[ii]],
                                     alpha = c(0.5, 1),
-                                    box.width = (250 - (jj * 5)),
+                                    box.width = (555 - (jj * 5)),
                                     cores = CORES,
-                                    verbose = FALSE,
-                                    test = FALSE)
+                                    verbose = FALSE)
         setTxtProgressBar(pb, ((ii - 1) * TESTS + jj))
     }
 }
@@ -30,3 +29,7 @@ r <- RunAccuracyTest(s, "../log", "BoxRatio", TESTS, ITERATIONS, COLNAMES)
 
 source("../func/VisualizeResults.R")
 VisualizeResults(r, s, "Ratio[Features Selected by Total Samples]", angle = 45)
+
+# Sim 3:
+# COLNAMES = seq(245, 15, -5) / 200
+# box.width = (250 - (jj * 5)),
