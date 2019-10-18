d5f9d8a3f9ded7dd8d07e08820a4db5910a920a9
James Matthew Hamilton <j.hamilton@gatech.edu>
2019-10-17 23:55:39 -0400

    Experiments will now check what the optimal cutoff value is for F1.

--- Tests/func/RunAccuracyTests.R
+++ Tests/func/RunAccuracyTests.R
@@ -25,17 +25,19 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
     rows = nrow(s$x[[1]])
     coefficients = s$coef
 
-    rmse <- rmseZ <- rmse2 <- f1 <- f2 <- dor <- TP <- FP <- FN <- TN <- TPR <- PPV <-
+    rmse <- rmseZ <- rmse2 <- f1 <- f2 <- dor <- TP <- FP <- FN <- TN <- TPR <- PPV <- f1.b <- f1.b.cutoff <-
     matrix(data = 0, nrow = iterations + 1, ncol = TESTS)
 
     coef.avg <- matrix(data = 0, nrow = nrow(coefficients[[1]]), ncol = TESTS)
     non.zero <- s$ground.truth != 0
 
-    if (!is.na(column.names)) {
+    if (!is.na(column.names[1])) {
         colnames(rmse) <- column.names
         colnames(rmseZ) <- column.names
         colnames(rmse2) <- column.names
         colnames(f1) <- column.names
+        colnames(f1.b) <- column.names
+        colnames(f1.b.cutoff) <- column.names
         colnames(f2) <- column.names
         colnames(dor) <- column.names
         colnames(coef.avg) <- column.names
@@ -48,6 +50,22 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
             rmse[ii,jj] <- RMSE(beta.hat, s$y.val[[ii]], s$x.val[[ii]])
             rmseZ[ii,jj] <- RMSE(beta.hat[non.zero], s$y.val[[ii]], s$x.val[[ii]][,non.zero])
             rmse2[ii,jj] <- RMSE2(s$ground.truth, beta.hat)
+            f1.trials = vector()
+            for (kk in 1:205) {
+                beta.hat.trial <- beta.hat
+                beta.hat.trial[abs(beta.hat) < 2 - ((kk - 1) * 0.0095)] <- 0
+                confusion.values <- mapply(FindConfusion, s$ground.truth, beta.hat.trial)
+                TP[ii,jj] <-.TP <- length(confusion.values[confusion.values == "TP"])
+                FP[ii,jj] <- .FP <- length(confusion.values[confusion.values == "FP"])
+                FN[ii,jj] <- .FN <- length(confusion.values[confusion.values == "FN"])
+                TN[ii,jj] <- .TN <- length(confusion.values[confusion.values == "TN"])
+                TPR[ii,jj] <- .TPR <- .TP / (.TP + .FN)
+                PPV[ii,jj] <- .PPV <- .TP / (.TP + .FP)
+                f1.trials[kk] <- 2 * (.PPV * .TPR) / (.PPV + .TPR)
+            }
+            f1.trials[is.na(f1.trials)] <- 0
+            f1.b.cutoff[ii, jj] <- 2 - ((min(which(f1.trials == max(f1.trials))) - 1) * 0.0095)
+            f1.b[ii,jj] <- max(f1.trials)
             
             confusion.values <- mapply(FindConfusion, s$ground.truth, beta.hat)
             TP[ii,jj] <-.TP <- length(confusion.values[confusion.values == "TP"])
@@ -56,7 +74,6 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
             TN[ii,jj] <- .TN <- length(confusion.values[confusion.values == "TN"])
             TPR[ii,jj] <- .TPR <- .TP / (.TP + .FN)
             PPV[ii,jj] <- .PPV <- .TP / (.TP + .FP)
-            
             f1[ii,jj] <- 2 * (.PPV * .TPR) / (.PPV + .TPR)
             f2[ii,jj] <- sqrt((.TP / (.TP + .FP)) * (.TP / (.TP + .FN)))
             dor[ii,jj] <- (.TP / .FP) / (.FN / .TN)
@@ -69,6 +86,8 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
         rmse[iterations + 1,] <- apply(rmse[1:iterations,], 2, mean)
         rmseZ[iterations + 1,] <- apply(rmseZ[1:iterations,], 2, mean)
         rmse2[iterations + 1,] <- apply(rmse2[1:iterations,], 2, mean)
+        f1.b[iterations + 1,] <- apply(f1.b[1:iterations,], 2, mean)
+        f1.b.cutoff[iterations + 1,] <- apply(f1.b.cutoff[1:iterations,], 2, mean)
         f1[iterations + 1,] <- apply(f1[1:iterations,], 2, mean)
         f2[iterations + 1,] <- apply(f2[1:iterations,], 2, mean)
         dor[iterations + 1,] <- apply(dor[1:iterations,], 2, mean)
@@ -76,6 +95,8 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
         rmse[iterations + 1,] <- mean(rmse[1:iterations,])
         rmseZ[iterations + 1,] <- mean(rmseZ[1:iterations,])
         rmse2[iterations + 1,] <- mean(rmse2[1:iterations,])
+        f1.b[iterations + 1,] <- mean(f1.b[1:iterations,])
+        f1.b.cutoff[iterations + 1,] <- mean(f1.b.cutoff[1:iterations,])
         f1[iterations + 1,] <- mean(f1[1:iterations,])
         f2[iterations + 1,] <- mean(f2[1:iterations,])
         dor[iterations + 1,] <- mean(dor[1:iterations,])
@@ -89,9 +110,11 @@ RunAccuracyTest <- function(s, dir, title, tests, iterations = 10, column.names)
     write.csv(x = rmse, paste0(directory, "/RMSE", rows.cols, ".csv"))
     write.csv(x = rmseZ, paste0(directory, "/Zero_RMSE", rows.cols, ".csv"))
     write.csv(x = rmse2, paste0(directory, "/Pure_RMSE", rows.cols, ".csv"))
-    write.csv(x = f1, paste0(directory, "/F1", rows.cols, ".csv"))
+    write.csv(x = f1.b, paste0(directory, "/F1", rows.cols, ".csv"))
+    write.csv(x = f1.b.cutoff, paste0(directory, "/Cutoff_F1", rows.cols, ".csv"))
+    write.csv(x = f1, paste0(directory, "/Pure_F1", rows.cols, ".csv"))
     write.csv(x = f2, paste0(directory, "/F2", rows.cols, ".csv"))
     write.csv(x = dor, paste0(directory, "/DOR", rows.cols, ".csv"))
     
-    return(list(rmse = rmse, rmseZ = rmseZ, rmse2 = rmse2, f1 = f1, f2 = f2, dor = dor, directory = directory))
+    return(list(rmse = rmse, rmseZ = rmseZ, rmse.pure = rmse2, f1 = f1.b, f1.cutoff = f1.b.cutoff, f1.pure = f1, f2 = f2, dor = dor, directory = directory))
 }
--- Tests/func/VisualizeResults.R
+++ Tests/func/VisualizeResults.R
@@ -17,15 +17,23 @@ VisualizeResults <- function(r, s, xlab, angle = 0) {
 
     png(paste0(directory, "/Image_Zero_RMSE", rows.cols, ".png"), width = 4096, height = 2160, pointsize = 40)
     par(mar = c(8,5,8,5))
-    plot <- barplot(r$rmseZ[iterations,], main = paste0("F1 [", rows, "x", cols, "] Date: ", format(Sys.time(), "%F@%H-%M-%S")),
+    plot <- barplot(r$rmseZ[iterations,], main = paste0("RMSE of Significant Values Only [", rows, "x", cols, "] Date: ", format(Sys.time(), "%F@%H-%M-%S")),
+                    axisnames = FALSE)
+    text(plot, par("usr")[3], labels = colnames(r$f1), srt = angle, adj = c(1.1,1.1), xpd = TRUE, cex = .9)
+    plot
+    dev.off()
+    
+    png(paste0(directory, "/Image_F1", rows.cols, ".png"), width = 4096, height = 2160, pointsize = 40)
+    par(mar = c(8,5,8,5))
+    plot <- barplot(r$f1[iterations,], main = paste0("F1 [", rows, "x", cols, "] Date: ", format(Sys.time(), "%F@%H-%M-%S")),
                     axisnames = FALSE)
     text(plot, par("usr")[3], labels = colnames(r$f1), srt = angle, adj = c(1.1,1.1), xpd = TRUE, cex = .9)
     plot
     dev.off()
 
-    png(paste0(directory, "/Image_F1", rows.cols, ".png"), width = 4096, height = 2160, pointsize = 40)
+    png(paste0(directory, "/Image_Pure_F1", rows.cols, ".png"), width = 4096, height = 2160, pointsize = 40)
     par(mar = c(8,5,8,5))
-    plot <- barplot(r$f1[iterations,], main = paste0("F1 [", rows, "x", cols, "] Date: ", format(Sys.time(), "%F@%H-%M-%S")),
+    plot <- barplot(r$f1.pure[iterations,], main = paste0("F1 (No Cutoff) [", rows, "x", cols, "] Date: ", format(Sys.time(), "%F@%H-%M-%S")),
             axisnames = FALSE)
     text(plot, par("usr")[3], labels = colnames(r$f1), srt = angle, adj = c(1.1,1.1), xpd = TRUE, cex = .9)
     plot
