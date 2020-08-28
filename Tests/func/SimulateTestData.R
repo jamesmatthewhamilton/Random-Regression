1dc813e98f36453c292ea7e939eccaefb8fb95c3
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-08-28 00:08:14 -0400

    Fixed index out of bounds bug.

--- Tests/func/SimulateTestData.R
+++ Tests/func/SimulateTestData.R
@@ -1,24 +1,24 @@
 SimulateTestData <- function(dataset, tests, iterations = 10) {
     load(dataset)
-    
+
     coef <- vector("list", tests)
     for (ii in 1:tests) {
         coef[[ii]] <- matrix(0, nrow = n_feature, ncol = iterations)
     }
-    
+
     y <- x <- y.test <- x.test <- y.val <- x.val <- cov.x <- ground.truth <- list()
     for (ii in 1:iterations) {
-        
+
         y[[ii]] <- matrix(unlist(sim_data[[ii]][2]), ncol = 1)
         x[[ii]] <- matrix(unlist(sim_data[[ii]][3]), ncol = n_feature, byrow = FALSE)
         y.val[[ii]] <- matrix(unlist(sim_data[[ii]][6]), ncol = 1)
         x.val[[ii]] <- matrix(unlist(sim_data[[ii]][7]), ncol = n_feature, byrow = FALSE)
         cov.x[[ii]] <- matrix(unlist(sim_data[[ii]][8]), ncol = n_feature, byrow = FALSE)
-        
-        colnames(x[[ii]]) <- colnames(x.val[[ii]]) <- colnames(x.test[[ii]]) <- paste0('Gene', seq(1:n_feature))
+
+        colnames(x[[ii]]) <- colnames(x.val[[ii]]) <- paste0('Gene', seq(1:n_feature))
     }
     ground.truth <- as.matrix(beta0)
-    
+
     return(list(coef = coef,
                 x = x,
                 y = y,
